aa5455c4e5c81652fd2df1ae521a9bb0fb2fcbc6
NikDoe <nikd0e@yandex.ru>
2023-07-11 19:41:31 +0300

    refactor: loading css as a separate file
    close #5

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,16 +1,19 @@
 import { RuleSetRule } from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-export function buildLoaders(): RuleSetRule[] {
-  const tsLoader = {
+import { BuildOptions } from './types/config';
+
+export function buildLoaders({ isDev }: BuildOptions): RuleSetRule[] {
+  const tsLoader: RuleSetRule = {
     test: /\.tsx?$/,
     use: 'ts-loader',
     exclude: /node_modules/,
   }
 
-  const cssLoader = {
+  const cssLoader: RuleSetRule = {
     test: /\.s[ac]ss$/i,
     use: [
-      "style-loader",
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
       "css-loader",
       "sass-loader",
     ],
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,13 +1,18 @@
 import { WebpackPluginInstance, ProgressPlugin } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { BuildPaths } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
+import { BuildPaths } from './types/config';
 
 export function buildPlugins({ html }: BuildPaths): WebpackPluginInstance[] {
   return [
     new ProgressPlugin(),
     new HtmlWebpackPlugin({
       template: html
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css", //куда складывать файлы, в билде
+      chunkFilename: "css/[name].[contenthash].css"
     }),
   ]
 }
