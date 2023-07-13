fac273a8bdf1ea1cf4330fbeec1b989cc9a5bc4f
NikDoe <nikd0e@yandex.ru>
2023-07-13 14:50:38 +0300

    fix: filter false value in additional arr

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type Mods = Record<string, boolean | string>;
 
-export function classNames(main: string, mods?: Mods, additionals?: string[]): string {
+export function classNames(main: string, mods: Mods = {}, additionals: string[] = []): string {
   return [
     main,
-    ...additionals,
+    ...additionals.filter(Boolean),
     ...Object.entries(mods)
       .filter(([className, value]) => Boolean(value))
       .map(([className]) => className)
