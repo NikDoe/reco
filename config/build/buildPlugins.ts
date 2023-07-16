9f981b94a4f143f7004af0417ee10e183461dfe0
NikDoe <nikd0e@yandex.ru>
2023-07-16 14:25:52 +0300

    config: add ```hot module replacement``` to webpack
    close #18

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,6 +1,12 @@
-import { WebpackPluginInstance, ProgressPlugin, DefinePlugin } from 'webpack';
+import {
+  WebpackPluginInstance,
+  ProgressPlugin,
+  DefinePlugin,
+  HotModuleReplacementPlugin
+} from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 import { BuildOptions } from './types/config';
 
@@ -14,9 +20,10 @@ export function buildPlugins({ paths, isDev }: BuildOptions): WebpackPluginInsta
       filename: "css/[name].[contenthash].css", //куда складывать файлы, в билде
       chunkFilename: "css/[name].[contenthash].css"
     }),
-
     new DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    new ReactRefreshWebpackPlugin(),
+    new HotModuleReplacementPlugin(),
   ]
 }
