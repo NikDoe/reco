d634da7fdf9affedb881558490488b247c25e66c
NikDoe <nikd0e@yandex.ru>
2023-07-17 12:21:13 +0300

    config: add jest
    add unit test for ```classNames``` helper
    
    close #21

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,34 @@
+import { classNames } from './classNames';
+
+describe('Тестирование хэлпера classNames', () => {
+    test('Добавление главного класса', () => {
+        expect(classNames('main')).toBe('main');
+    });
+
+    test('Добавление дополнительных классов', () => {
+        const expected = 'main класс-1 класс-2';
+        expect(classNames('main', {}, ['класс-1', 'класс-2'])).toBe(expected);
+    });
+
+    test('Добавление классов модов', () => {
+        const mods = {
+            hovered: true,
+            selected: true,
+        };
+
+        const expected = 'main класс-1 класс-2 hovered selected';
+
+        expect(classNames('main', mods, ['класс-1', 'класс-2'])).toBe(expected);
+    });
+
+    test('Классы моды со значением false', () => {
+        const mods = {
+            hovered: true,
+            selected: false,
+        };
+
+        const expected = 'main класс-1 класс-2 hovered';
+
+        expect(classNames('main', mods, ['класс-1', 'класс-2'])).toBe(expected);
+    });
+});
